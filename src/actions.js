ace2d839a4afe62a8db5b7c6a531ccaee8382587
tharunkarnekota <Tharunkarnekota@gmail.com>
2021-09-30 23:32:57 +0530

    cart using react redux concept

---
+++ src/actions.js
@@ -0,0 +1,13 @@
+export const AddProduct = (value) => async dispatch => {
+    dispatch({
+        type: "ADD",
+        payload: value
+    })
+}
+
+export const RemoveProduct = (value) => async dispatch =>{
+    dispatch({
+        type: "REMOVE",
+        payload: {name:value}
+    })
+}
